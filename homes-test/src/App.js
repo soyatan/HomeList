919c52ca23b986959ea6fa6bfc579fa744504086
Necati Soyata <necati.soyata@gmail.com>
2021-07-25 22:12:09 +0300

    "set navbar and jest"

---
+++ homes-test/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Homes from "./Homes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Homes />
+    </div>
+  );
+}
+
+export default App;
---
+++ homes-test/src/Homes.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Homes() {
+  return <div>Header!</div>;
+}
